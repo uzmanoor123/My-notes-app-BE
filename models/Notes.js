ff4453f94774e89c0ec5fa4d440ad2045c64579f
uzma <uzmanoor200301@gmail.com>
2026-07-08 20:56:05 +0500

    setup mongodb

---
+++ models/Notes.js
@@ -0,0 +1,21 @@
+const mongoose = require ("mongoose");
+const noteSchema = new mongoose.Schema({
+    title:{
+       type:String,
+        required:true
+    },
+       description:{
+       type:String,
+        required:true
+    },
+      createdAt:{
+       type:Date,
+        default: Date.now()
+    },
+      updatedAt:{
+       type: Date,
+        default: Date.now()
+    },
+})
+const Note = mongoose.model("Notes", noteSchema);
+module.exports = Note;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 app.use(express.json());
@@ -65,6 +67,10 @@ app.delete('/api/notes/:id', (req,res)=>{
   notes.splice(index, 1)
   res.send(note);
 })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app is running on port  ${port}`);
